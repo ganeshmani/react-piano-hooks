3e0215171e9803f0134bc15908db6ab5285ecb56
Timeless Ventures <ganeshmani009@gmail.com>
2020-01-30 23:39:09 +0530

    changes

--- src/Piano.js
+++ src/Piano.js
@@ -60,34 +60,46 @@ const Piano = () => {
     <div className="piano-container">
       <Instrument
         instrumentName={"acoustic_grand_piano"}
-        startNote={"C4"}
+        startNote={"C3"}
         endNote={"B5"}
         renderPianoKey={renderPianoKey}
         keyboardMap={{
-          Q: "C4",
-          2: "C#4",
-          W: "D4",
-          3: "D#4",
-          E: "E4",
-          R: "F4",
-          5: "F#4",
-          T: "G4",
-          6: "G#4",
-          Y: "A4",
-          7: "A#4",
-          U: "B4",
-          V: "C5",
-          G: "C#5",
-          B: "D5",
-          H: "D#5",
-          N: "E5",
-          M: "F5",
-          K: "F#5",
-          ",": "G5",
-          L: "G#5",
-          ".": "A5",
-          ";": "A#5",
-          "/": "B5"
+          Q: "C3",
+          2: "C#3",
+          W: "D3",
+          3: "D#3",
+          E: "E3",
+          R: "F3",
+          5: "F#3",
+          T: "G3",
+          6: "G#3",
+          Y: "A3",
+          7: "A#3",
+          U: "B3",
+          I: "C4",
+          9: "C#4",
+          O: "D4",
+          0: "D#4",
+          P: "E4",
+          Z: "F4",
+          S: "F#4",
+          X: "G4",
+          D: "G#4",
+          C: "A4",
+          F: "A#4",
+          V: "B4",
+          B: "C5",
+          H: "C#5",
+          N: "D5",
+          J: "D#5",
+          M: "E5",
+          ",": "F5",
+          L: "F#5",
+          ".": "G5",
+          ";": "G#5",
+          "/": "A5",
+          "'": "A#5",
+          A: "B5"
         }}
       />
     </div>

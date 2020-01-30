9704b842f4d4cf381c7983349cf69a854415d75a
Timeless Ventures <ganeshmani009@gmail.com>
2020-01-30 22:13:25 +0530

    Updates

---
+++ precache-manifest.052868696d8a7171e04c038209ce8657.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06f2960a41aaed72f7a913bbbdf56e19",
+    "url": "/piano-hooks/index.html"
+  },
+  {
+    "revision": "36f8fe2da1f34153b7e3",
+    "url": "/piano-hooks/static/css/main.2ea270a6.chunk.css"
+  },
+  {
+    "revision": "8c2ab5734eb240732a5f",
+    "url": "/piano-hooks/static/js/2.716792c0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/piano-hooks/static/js/2.716792c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "36f8fe2da1f34153b7e3",
+    "url": "/piano-hooks/static/js/main.8fb5b8d1.chunk.js"
+  },
+  {
+    "revision": "8a1921252fbc5321a57c",
+    "url": "/piano-hooks/static/js/runtime-main.54b490e3.js"
+  }
+]);
